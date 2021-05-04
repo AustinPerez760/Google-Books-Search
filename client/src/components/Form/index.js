65ef159ea0543718d382fe2940ba979c817651c3
Austin Perez <austin.perez760@gmail.com>
2021-05-03 18:01:15 -0700

    began From component

---
+++ client/src/components/Form/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function TextInput(props) {
+  return <input {...props} />;
+}
+
+export function FormBtn(props) {
+  return <button {...props}>{props.children}</button>;
+}
